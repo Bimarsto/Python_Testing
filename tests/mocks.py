ba687c0945df7bf836a53922a0d62acbab1c7f46
Bimarsto <90108221+Bimarsto@users.noreply.github.com>
2023-01-20 17:07:13 +0100

    Create mocks.py

---
+++ tests/mocks.py
@@ -0,0 +1,40 @@
+def mock_clubs_data():
+    clubs = [
+        {
+            "name": "First Club",
+            "email": "firstclub@test.com",
+            "points": "10",
+        },
+        {
+            "name": "Second Club",
+            "email": "secondclub@test.com",
+            "points": "5",
+        },
+        {
+            "name": "Third Club",
+            "email": "thirdclub@test.com",
+            "points": "2",
+        },
+    ]
+    return clubs
+
+
+def mock_competitions_data():
+    competitions = [
+        {
+            "name": "First Competition",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "16",
+        },
+        {
+            "name": "Second Competition",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "8",
+        },
+        {
+            "name": "Third Competition",
+            "date": "2023-01-22 15:30:00",
+            "numberOfPlaces": "2",
+        },
+    ]
+    return competitions
